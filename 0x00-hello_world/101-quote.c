a1890bdfad7d81bf23727313db8d92c86024a784
wilsonw1998 <ahilda198@gmail.com>
2023-02-16 19:27:19 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * main -  A program that prints a line to the standard error
+ *
+ * Return: 1 (Success)
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
